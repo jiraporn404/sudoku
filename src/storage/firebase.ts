ac1c05ef6c8bce0ec8d1a8d9e832f392df751db2
jiraporn404 <chm.jiraporn@gmail.com>
2025-07-10 14:06:14 +0700

    Refactor Firebase configuration.

--- src/storage/firebase.ts
+++ src/storage/firebase.ts
@@ -7,14 +7,14 @@ import { getDatabase } from "firebase/database";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyAtFKdiOrvJgElOyfqttD9ef1CyoG-8h3A",
-  authDomain: "sudoku-b03d2.firebaseapp.com",
-  databaseURL: "https://sudoku-b03d2-default-rtdb.firebaseio.com",
-  projectId: "sudoku-b03d2",
-  storageBucket: "sudoku-b03d2.firebasestorage.app",
-  messagingSenderId: "423149871061",
-  appId: "1:423149871061:web:bf375323258166d9ca275c",
-  measurementId: "G-JHT709XY8G",
+  apiKey: import.meta.env.VITE_API_KEY,
+  authDomain: import.meta.env.VITE_AUTH_DOMAIN,
+  databaseURL: import.meta.env.VITE_DATABASE_URL,
+  projectId: import.meta.env.VITE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_APP_ID,
+  measurementId: import.meta.env.VITE_MEASUREMENT_ID,
 };
 
 // Initialize Firebase
